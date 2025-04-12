dc44e9e33c6ba9783a2c12fbc78357d07b589e14
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-04-12 13:09:55 +0100

    feat: added stages and job running

--- src/cip/jobs.py
+++ src/cip/jobs.py
@@ -1,16 +1,24 @@
-from typing import Dict, List, Self
+from typing import Dict, List, Self, Callable
 
 
 class BaseJob:
     def __init__(self, name: str):
         self.name = name
         self.depends_on = []
+        self.stages = []
+
+    def stage(self, stage: Callable):
+        self.stages.append(stage)
 
     def depends(self, dependencies: List[Self]):
         self.depends_on = dependencies
 
     def run(self, context: Dict):
-        pass
+        for stage in self.stages:
+            if stage(context) == False:
+                return False
+        
+        return True
 
 
 class Job(BaseJob):
