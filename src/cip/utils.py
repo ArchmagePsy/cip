61437a57751e2fec3b73cfbbc9bc096518827682
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-04-16 00:39:32 +0100

    feat: added support for a basic shell

--- src/cip/utils.py
+++ src/cip/utils.py
@@ -1,19 +1,22 @@
 import functools
+from os import PathLike
 import subprocess
 from typing import Callable, Dict, List
 
 
-def process(fail_fast: bool = True):
+def process(fail_fast: bool = True, shell: bool = False, executable: PathLike | None = None):
     def process_decorator(step: Callable):
         result = True
 
         def process_patch(cmd: str, *args: List[str]):
             nonlocal result
-            completed_process: subprocess.CompletedProcess =  subprocess.run([cmd, *args])
+            completed_process: subprocess.CompletedProcess =  subprocess.run([cmd, *args], shell=shell, executable=executable)
             if fail_fast:
                 completed_process.check_returncode()
             else:
                 result = result and (completed_process.returncode == 0)
+
+            return completed_process
 
         @functools.wraps(step)
         def process_wrapper(context: Dict):
@@ -28,3 +31,9 @@ def process(fail_fast: bool = True):
 
 def process_default(step: Callable):
     return process()(step)
+
+def shell(fail_fast: bool = True, executable: PathLike | None = None):
+    return process(fail_fast=fail_fast, shell=True, executable=executable)
+
+def shell_default(step: Callable):
+    return process(shell=True)(step)
