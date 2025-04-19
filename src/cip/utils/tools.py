4fdbf2774170c6439251bebc638d544e13614d72
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-04-19 07:00:14 +0100

    style: changed utils to tools

--- src/cip/utils/tools.py
+++ src/cip/utils/tools.py
@@ -2,7 +2,6 @@ import functools
 import inspect
 from os import PathLike
 import os
-import shutil
 import subprocess
 import tempfile
 from typing import Callable, Dict, List
@@ -42,10 +41,10 @@ def shell(fail_fast: bool = True, executable: PathLike | None = None):
 def shell_default(step: Callable):
     return process(shell=True)(step)
 
-def script(shell_executable: PathLike):
+def script(interpreter: PathLike):
     def script_decorator(step: Callable):
         script_text = inspect.getdoc(step)
-        script_text = f"#!{shell_executable}\n\n" + script_text
+        script_text = f"#!{interpreter}\n\n" + script_text
 
         @functools.wraps(step)
         def script_wrapper(context: Dict):
@@ -53,7 +52,7 @@ def script(shell_executable: PathLike):
             script_temp.write(script_text.encode())
             script_temp.seek(0)
             os.chmod(script_temp.name, 0o1700)
-            completed_process: subprocess.CompletedProcess = subprocess.run(f"{script_temp.name}", shell=True, executable=shell_executable)
+            completed_process: subprocess.CompletedProcess = subprocess.run(f"{script_temp.name}", shell=True, executable=interpreter)
             return completed_process.returncode == 0
         
         return script_wrapper
