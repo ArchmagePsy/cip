a1ace1b0ee2f27d8b3d734ba6f41ddf3a80547d7
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-04-27 13:07:55 +0100

    test: implemented pipeline sync and async run tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,3 +1,5 @@
+import asyncio
+from typing import Dict
 from cip import pipelines, stages
 
 
@@ -15,3 +17,62 @@ def test_pipeline_repr():
 
     assert repr(dummy_pipeline) == "Pipeline(dummy_stage, dummy_stage1)"
 
+def test_pipeline_run():
+    dummy_pipeline = pipelines.Pipeline()
+
+    @dummy_pipeline.stage2.job1.step
+    def step4(context: Dict):
+        assert context["RESULTS"][dummy_pipeline.stage1.job1] and context["RESULTS"][dummy_pipeline.stage1.job2]
+
+    @dummy_pipeline.stage1.job1.step
+    def step1(context: Dict):
+        assert context["PIPELINE"] == dummy_pipeline
+        assert context["JOB"] == dummy_pipeline.stage1.job1
+        assert context["STEP"] == "step1"
+        assert "RESULTS" in context
+        return False
+
+    @dummy_pipeline.stage1.job1.step
+    def step2(context: Dict):
+        assert False
+
+    dummy_pipeline.stage1.job1.allow_fail()
+
+    @dummy_pipeline.stage1.job2.step
+    def step3(context: Dict):
+        assert context["RESULTS"][dummy_pipeline.stage1.job1]
+
+    dummy_pipeline.stage2.job1.depends(dummy_pipeline.stage1)
+
+    dummy_pipeline.run()
+
+def test_pipeline_run_async():
+    dummy_pipeline = pipelines.AsyncPipeline()
+
+    @dummy_pipeline.stage2.job1.step
+    def step4(context: Dict):
+        assert context["RESULTS"][dummy_pipeline.stage1.job1] and context["RESULTS"][dummy_pipeline.stage1.job2]
+
+    @dummy_pipeline.stage1.job1.step
+    def step1(context: Dict):
+        assert context["PIPELINE"] == dummy_pipeline
+        assert context["JOB"] == dummy_pipeline.stage1.job1
+        assert context["STEP"] == "step1"
+        assert "RESULTS" in context
+        return False
+
+    @dummy_pipeline.stage1.job1.step
+    def step2(context: Dict):
+        assert False
+
+    dummy_pipeline.stage1.job1.allow_fail()
+
+    @dummy_pipeline.stage1.job2.step
+    def step3(context: Dict):
+        assert context["RESULTS"][dummy_pipeline.stage1.job1]
+
+    dummy_pipeline.stage2.job1.depends(dummy_pipeline.stage1)
+    # this prevents race condition between job1 and job2 in stage1
+    dummy_pipeline.stage1.job2.depends(dummy_pipeline.stage1.job1)
+
+    asyncio.run(dummy_pipeline.run())
