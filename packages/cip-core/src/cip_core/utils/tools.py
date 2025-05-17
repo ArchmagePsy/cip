bfc4c79399a7326ffb964743efa847e88a02e437
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-05-17 10:59:03 +0100

    docs: added documentation for tools module

--- packages/cip-core/src/cip_core/utils/tools.py
+++ packages/cip-core/src/cip_core/utils/tools.py
@@ -1,3 +1,8 @@
+"""
+This module contains decorators that can be added to functions in order to expose tools to your methods. A tool is essentially a dependency that you
+inject into your jobs which allows you to call some external process or module. The most common use case of tools is providing chell access to your jobs
+in order to run external build systems, testing solutions, etc.
+"""
 import functools
 import inspect
 from os import PathLike
@@ -8,6 +13,23 @@ from typing import Callable, Dict, List
 
 
 def process(fail_fast: bool = True, shell: bool = False, executable: PathLike | None = None):
+    """
+    This tool wraps a job step and passes a process dependency as the first argument.
+    Calling the dependency executes the given command in a separate subprocess.
+
+    Args:
+        fail_fast(bool): determines whether or not the job step should fail as soon as a call to process
+            fails with a nonzero returncode (breaking the step's execution) or instead keep executing until
+            the step is finished and return a failure only then. (this defaults to True)
+
+        shell(bool): determines whether or not to execute the subprocesses in a shell context. (This defaults to False)
+        
+        executable(PathLike): the executable to pass commands to, defaults to whatever the default system shell
+            interpreter is.
+
+    Returns:
+        Callable: the decorator used to wrap the step definition.
+    """
     def process_decorator(step: Callable):
         result = True
         environment = os.environ.copy()
@@ -37,15 +59,47 @@ def process(fail_fast: bool = True, shell: bool = False, executable: PathLike | 
     return process_decorator
 
 def process_default(step: Callable):
+    """
+    This is the default configuration of the process decorator.
+    """
     return process()(step)
 
 def shell(fail_fast: bool = True, executable: PathLike | None = None):
+    """
+    This tool wraps the job step with a shell allowing it to execute more complex commands. It functions
+    as a reconfiguration of the process tool decorator.
+
+    Args:
+        fail_fast(bool): determines whether or not the job step should fail as soon as a call to process
+            fails with a nonzero returncode (breaking the step's execution) or instead keep executing until
+            the step is finished and return a failure only then. (this defaults to True)
+
+        executable(PathLike): the executable to pass commands to, defaults to whatever the default system shell
+            interpreter is.
+
+    Returns:
+        Callable: the decorator used to wrap the step definition.
+    """
     return process(fail_fast=fail_fast, shell=True, executable=executable)
 
 def shell_default(step: Callable):
+    """
+    This is the default configuration of the shell decorator.
+    """
     return process(shell=True)(step)
 
 def script(interpreter: PathLike):
+    """
+    This tool wraps a job step and extracts the docstring attached to it writing it out to a temporary file
+    and setting it as executable. The tool then executes this script using the given interpreter. This way you
+    can (almost) seemlessly integrate in-place scripts using external interpreters.
+
+    Args:
+        interpreter(PathLike): the path to the interpreter used to execute the script.
+
+    Returns:
+        Callable: the decorator used to wrap the step definition.
+    """
     def script_decorator(step: Callable):
         script_text = inspect.getdoc(step)
         script_text = f"#!{interpreter}\n\n" + script_text
@@ -66,7 +120,13 @@ def script(interpreter: PathLike):
     return script_decorator
 
 def bash(step: Callable):
+    """
+    This is a configuration of the script tool that executes scripts using the Bourne Again SHell (BASH) interpreter.
+    """
     return script("/bin/bash")(step)
 
 def sh(step: Callable):
+    """
+    This is a configuration of the script tool that executes scripts using the system's default shell through 'sh'.
+    """
     return script("/bin/sh")(step)
