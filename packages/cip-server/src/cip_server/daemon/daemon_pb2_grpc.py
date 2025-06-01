bab244ebef103a749284d35844fd3f28322d10c7
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-06-01 09:27:25 +0100

    fix: imported daemon types from package

--- packages/cip-server/src/cip_server/daemon/daemon_pb2_grpc.py
+++ packages/cip-server/src/cip_server/daemon/daemon_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import daemon_pb2 as daemon__pb2
+from cip_server.daemon import daemon_pb2 as daemon__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
