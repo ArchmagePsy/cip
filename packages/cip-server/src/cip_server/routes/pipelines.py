62b0918080daa8c1e25196e8579dd60c8f94eb29
ArchmagePsy <SeaOfCodeSi@gmail.com>
2025-06-07 10:22:28 +0100

    docs: add swagger documentation for basic pipeline api

--- packages/cip-server/src/cip_server/routes/pipelines.py
+++ packages/cip-server/src/cip_server/routes/pipelines.py
@@ -9,19 +9,25 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 
 
-pipeline_router = APIRouter()
+pipeline_router = APIRouter(tags=["Pipeline Executions"])
 
-@pipeline_router.get("/pipeline/{pipeline_id}")
+@pipeline_router.get("/pipeline/{pipeline_id}", 
+                     summary="Get a pipeline execution object", 
+                     description="Retrieve all the details for a pipeline execution with the given id")
 async def get_pipeline(pipeline_id: uuid.UUID, session: AsyncSession = Depends(get_session)):
     pipeline_execution = await session.get(PipelineExecution, pipeline_id)
     return pipeline_execution
     
-@pipeline_router.get("/pipeline/{pipeline_id}/result/{index}")
+@pipeline_router.get("/pipeline/{pipeline_id}/result/{index}",
+                     summary="Get a job result object",
+                     description="Retrieve the results of jobs in a pipeline using its id and index")
 async def get_job_result(pipeline_id: uuid.UUID, index: int, session: AsyncSession = Depends(get_session)):
     pipeliene_execution = await session.get(PipelineExecution, pipeline_id, options=[selectinload(PipelineExecution.job_results)])
     return pipeliene_execution.job_results[index]
 
-@pipeline_router.post("/pipeline")
+@pipeline_router.post("/pipeline",
+                      summary="Create a new pipeline execution",
+                      description="Run the pipeline defined in the repository at the given url and at the point of the given commit. Returns the unique id of the new pipeline")
 async def create_pipeline(git_url: str, commit_hash: str, stub: PipelineExecutorStub = Depends(get_pipeline_executor_stub)):
     response = await stub.ExecutePipeline(PipelineExecutionRequest(git_url=git_url, commit_hash=commit_hash))
     return response.pipeline_execution_id
